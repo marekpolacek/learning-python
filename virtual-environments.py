bb46d0079b88fac1ce86629ec9c36c9ab7d0c4e1
Marek Polacek <mapo@onemarketing.dk>
2020-03-11 00:19:18 +0100

    virtual environments

---
+++ virtual-environments.py
@@ -0,0 +1,19 @@
+# working with virtual environments
+
+## create a new empty project
+# mkdir my_project
+
+## create a virtual environment for the project
+# python -m venv my_project/venv
+
+## activate the virtual environment
+# source my_project/venv/bin/activate
+
+## install required modules to the new virtual environment
+# pip install -r requirements.txt
+
+## list modules in the current virtual environment
+# pip list
+
+## deactivate the current virtual environment
+# deactivate
